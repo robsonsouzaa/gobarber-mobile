bfdfaac13f73d7685fc4235a74fe071117778afa
Robson Souza <robsonex@gmail.com>
2020-09-15 16:54:33 -0400

    feat: realiza o agendamento de acordo com provider

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -17,7 +17,7 @@ const AppRoutes: React.FC = () => (
   >
     <App.Screen name="Dashboard" component={Dashboard} />
     <App.Screen name="CreateAppointment" component={CreateAppointment} />
-    <App.Screen name="AppointmenteCreated" component={AppointmenteCreated} />
+    <App.Screen name="AppointmentCreated" component={AppointmenteCreated} />
 
     <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
