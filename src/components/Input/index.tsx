3ba349e34b623c4bc79cb040c0fd4e2185821213
Robson Souza <robsonex@gmail.com>
2020-09-17 11:35:12 -0400

    interface: cria página de alteração perfil

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,14 +6,15 @@ import React, {
   forwardRef,
   useCallback,
 } from 'react';
-import { TextInputProperties } from 'react-native';
+import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
-interface InputProps extends TextInputProperties {
+interface InputProps extends TextInputProps {
   name: string;
   icon: string;
+  containerStyle?: {};
 }
 
 interface InputValueReference {
@@ -25,7 +26,7 @@ interface InputRef {
 }
 
 const Input: React.RefForwardingComponent<InputRef, InputProps> = (
-  { name, icon, ...rest },
+  { name, icon, containerStyle = {}, ...rest },
   ref,
 ) => {
   const inputElementRef = useRef<any>(null);
@@ -69,7 +70,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused} isErrored={!!error}>
+    <Container style={containerStyle} isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={20}
